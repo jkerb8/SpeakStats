799d6c40dab4eed71ef66efef13b932b15d34836
jkerb8 <jonathankerbelis@gmail.com>
2016-01-05 19:49:07 -0500

    NOT Working- attempting to write to .csv files

--- app/src/main/java/com/kerbysoft/qwikcut/speakstats/MainActivity.java
+++ app/src/main/java/com/kerbysoft/qwikcut/speakstats/MainActivity.java
@@ -6,12 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.RecognitionService;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
@@ -19,12 +30,23 @@ public class MainActivity extends Activity {
     protected static final int RESULT_SPEECH = 1;
 
     private ImageButton btnSpeak;
+    private Button export;
     private TextView txtText;
+    public ArrayList<String> playList;
+    public File writer;
+    int counter = 0;
+    String csvname = "play_list.csv";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playList = new ArrayList<String>();
+
+        File directory = getFilesDir();
+        writer = new File(directory, csvname);
+
 
         txtText = (TextView) findViewById(R.id.txtText);
 
@@ -74,10 +96,53 @@ public class MainActivity extends Activity {
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
                     txtText.setText(text.get(0));
+                    playList.add(text.get(0));
+                    writer.println(text.get(0) + " , " + String.valueOf(++counter));
                 }
                 break;
             }
 
         }
     }
+
+    private void copyFile(String inputPath, String inputFile, String outputPath) {
+
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+
+            //create output directory if it doesn't exist
+            File dir = new File (outputPath);
+            if (!dir.exists())
+            {
+                dir.mkdirs();
+            }
+
+
+            in = new FileInputStream(inputPath + inputFile);
+            out = new FileOutputStream(outputPath + inputFile);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            in = null;
+
+            // write the output file (You have now copied the file)
+            out.flush();
+            out.close();
+            out = null;
+
+        }  catch (FileNotFoundException fnfe1) {
+            Log.e("tag", fnfe1.getMessage());
+        }
+        catch (Exception e) {
+            Log.e("tag", e.getMessage());
+        }
+
+    }
+
+    //copyFile(currentDirectory, "play_list.csv", DownloadsFolder);
 }
